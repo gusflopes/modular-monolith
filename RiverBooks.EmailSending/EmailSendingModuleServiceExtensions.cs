75876a7edd3c56f3d5f0993e5e2bb1d473ccf0cb
Gustavo Lopes <gusflopes86@gmail.com>
2024-04-22 20:23:36 -0400

    ammend: email sending

---
+++ RiverBooks.EmailSending/EmailSendingModuleServiceExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace RiverBooks.EmailSending;
+
+public static class EmailSendingModuleServiceExtensions
+{
+  public static IServiceCollection AddEmailSendingModuleServices(
+      this IServiceCollection services,
+      ConfigurationManager config,
+      ILogger logger,
+      List<System.Reflection.Assembly> mediatRAssemblies)
+    {
+      // Add Module services
+      services.AddTransient<ISendEmail, MimeKitEmailSender>();
+      
+      mediatRAssemblies.Add(typeof(EmailSendingModuleServiceExtensions).Assembly);
+      
+      logger.Information("{Module} module services registered", "Email Sending");
+      
+      return services;
+    }
+}
